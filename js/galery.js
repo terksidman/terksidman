1071e6fff7abe9b001c579e195c1b11ea138b3db
terksidman <44986526+terksidman@users.noreply.github.com>
2018-11-14 01:32:35 +0300

    Add files via upload

---
+++ js/galery.js
@@ -0,0 +1,34 @@
+
+{
+	var mas = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg"] ;// массив картинок
+	var to = -1;  // Счетчик, указывающий на текущую картинки
+	var url = "background: url(pictures/";
+	var end = "";
+	var obj = document.getElementById("img");
+	window.onload = function autoscroll(){
+		
+	  	if (to < mas.length-1)  to++ 
+	   	else
+	     to = 0;
+	     obj.style = url + mas[to]+'';
+	     obj.style.backgroundSize = "100%";
+	}
+	function right_arrow() // Открытие следующей картинки(движение вправо)
+	{ 
+	
+	  if (to < mas.length-1)  to++ 
+	   else
+	     to = 0;
+	     obj.style = url + mas[to]+'';
+	     obj.style.backgroundSize = "100%";
+	}
+
+	function left_arrow() // Открытие предыдущей картинки(движение влево)
+	{     
+
+	if (to > 0) to--;
+	  else
+	    to = mas.length-1;
+	    obj.style = mas[to];	  			 
+	}
+}
