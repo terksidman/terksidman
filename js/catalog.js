c1be1fee808af385eeaa07be0d6bd92ab2cda0f1
terksidman <44986526+terksidman@users.noreply.github.com>
2018-11-15 21:29:39 +0300

    Add files via upload

--- js/catalog.js
+++ js/catalog.js
@@ -1,33 +1,57 @@
 {
-	var prevpage ;
+	var age ;
 	var nextpage ;
 
 	function left_arrow(){
-		if(prevpage==0)
+		page --;
+		nextpage --;
+		if(page==0 && nextpage == 1)
 		{
-		$('#sex').fadeTo(100, 1);
-		$('#male-belts').css('display', 'none');
-		$('.arrow-box-left').fadeTo(200, 0);
-		$('.arrow-box-right').fadeTo(200, 1);
-
-		prevpage = 0;
-		nextpage = 1;
+			$('#sex').fadeTo(100, 1);
+			$('#male-belts').css('display', 'none');
+			$('.arrow-box-left').fadeTo(200, 0);
+			$('.arrow-box-right').fadeTo(200, 1);
 		}
+		if (page==10 && nextpage == 21) {
+			$('#sex').fadeTo(100, 1);
+			$('#female-belts').css('display', 'none');
+			$('.arrow-box-left').fadeTo(200, 0);
+			$('.arrow-box-right').fadeTo(200, 1);
+		}
+		
 	}
 	function right_arrow(){
+		page ++;
+		nextpage ++;
+		if(page==1 && nextpage == 2){
+			$('#sex').css('display', 'none');
+			$('#male-belts').fadeTo(200, 1);
+			$('.arrow-box-left').fadeTo(200, 1);
+			$('.arrow-box-right').fadeTo(200, 0);
+		}
+		if(page==11 && nextpage == 22){
+			$('#sex').css('display', 'none');
+			$('#female-belts').fadeTo(200, 1);
+			$('.arrow-box-left').fadeTo(200, 1);
+			$('.arrow-box-right').fadeTo(200, 0);
+		}
 	}
 	$('#male').on('click', function(){
 		$('#sex').fadeTo(100, 0);
 		$('#male-belts').fadeTo(200, 1);
 		$('.arrow-box-left').fadeTo(200, 1);
-		prevpage=0;
+		$('.arrow-box-right').fadeTo(200, 0);
+		page=1;
+		nextpage=2;
 
 	});
 	$('#female').on('click', function(){
 		$('#sex').fadeTo(100, 0);
 		$('#female-belts').fadeTo(200, 1);
 		$('.arrow-box-left').fadeTo(200, 1);
-		prevpage=0;
+		$('.arrow-box-right').fadeTo(200, 0);
+		page=11;
+		nextpage=22;
 
 	});
 
